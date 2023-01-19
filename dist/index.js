3ba1a8a0a581ed14701ea0e2abfd7db4343d6144
Orebayo Oluwapelumi <orebayopelumi@gmail.com>
2023-01-19 15:29:26 +0100

    express setup

--- dist/index.js
+++ dist/index.js
@@ -1,72 +1,31 @@
 "use strict";
-;
-const result = 2;
-console.log(result);
-const data = [1, "2"];
-console.log(data);
-function calculateTax(income, taxYear = 2022) {
-    if (taxYear < 2020)
-        return income * 2;
-    return income * 1.3;
-}
-let employee = { name: "Oluwapelumi" };
-console.log(employee);
-function kgToLbs(weight) {
-    if (typeof weight === "number") {
-        return weight * 2.2;
-    }
-    else {
-        return parseInt(weight) * 2;
-    }
-}
-const d2 = kgToLbs(10);
-console.log(d2);
-kgToLbs("20kg");
-let digit = 50;
-console.log(digit);
-const navComponent = {
-    drag: () => {
-        console.log("Hello");
-    },
-    resize: () => {
-        console.log("Resize");
-    }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-console.log(navComponent);
-function greet(name) {
-    if (typeof name === "string") {
-        console.log(name.toUpperCase());
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)({ path: "./.env" });
+const http_1 = __importDefault(require("http"));
+const app_1 = __importDefault(require("./app"));
+const normalizePort = (value) => {
+    const port = parseInt(value, 10);
+    if (isNaN(port)) {
+        return value;
     }
-    else
-        console.log("Holla!");
-}
-greet(null);
-function getCustomer(id) {
-    return id === 0 ? null : { name: "Oluwapelumi", id };
-}
-const customerDto = getCustomer(0);
-console.log(customerDto === null || customerDto === void 0 ? void 0 : customerDto.name);
-let log = null;
-class Account {
-    constructor(id, holder, balance) {
-        this.id = id;
-        this.holder = holder;
-        this._balance = balance;
+    if (port >= 0) {
+        return port;
     }
-    deposit(amount) {
-        if (amount < 0)
-            throw new Error('Invalid Amount');
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
-}
-const account = new Account(0, "Orebayo Oluwapelumi", 40000);
-account.deposit(1000);
-class seatAssignment {
-}
-const seats = new seatAssignment();
-seats.A2 = "pelumi";
-console.log(seats);
+    return false;
+};
+const server = http_1.default.createServer(app_1.default);
+const port = normalizePort(process.env.PORT);
+server.on("listening", () => {
+    const address = server.address();
+    console.log(typeof address);
+    const bind = typeof address === "string" ? `pipe ${port}` : `port ${port}`;
+    console.log(`listening on ${bind}`);
+    const log = "[?...] Connecting";
+    console.log(log);
+});
+server.listen(port);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,116 +1,30 @@
-// const enum Size {Small=1, Medium,Large};
-// const result: Size= Size.Medium;
-// console.log(result);
+import { config } from "dotenv";
+config({path: "./.env"});
+import http from "http";
+import app from "./app";
+/**
+ * 
+ * @param value string
+ * @returns number | string | boolean
+ */
+const normalizePort = (value: string): number | string | boolean => {
+  const port = parseInt(value, 10);
+  if (isNaN(port)) {
+    return value;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
 
-// const data: [number,string]= [1,"2"];
-// console.log(data);
-
-// function calculateTax(income: number,taxYear=2022): number{
-//     if(taxYear < 2020) return income * 2;
-//     return income * 1.3
-// }
-
-// type Employee = {
-//     name: string
-// }
-
-// let employee: Employee = {name: "Oluwapelumi"}
-// console.log(employee)
-
-// function kgToLbs(weight: number | string){
-//     // Narrowing
-//  if(typeof weight === "number"){
-//    return weight * 2.2;
-//  }
-//  else {
-//     return parseInt(weight) * 2;
-//  }
-
-// }
-// const d2= kgToLbs(10);
-// console.log(d2);
-// kgToLbs("20kg");
-
-
-// // Literal Types
-// let digit: 50= 50;
-// console.log(digit);
-
-
-// // Intersection
-// type Dragable ={ 
-//     drag: ()=> void
-// }
-
-// type ResizeAble= {
-//     resize: ()=> void
-// }
-
-// type UIWidget= Dragable & ResizeAble;
-// const navComponent: UIWidget = {
-//      drag: ()=> {
-//         console.log("Hello")
-//      },
-//      resize: ()=>{
-//         console.log("Resize")
-//      }
-// }
-
-// console.log(navComponent)
-
-
-// function greet(name: string | null){
-//  if(typeof name === "string"){
-//     console.log(name.toUpperCase())
-//  }else 
-//  console.log("Holla!");
-// }
-
-// greet(null);
-
-// type Customer= {
-//     name: string
-//     id: number
-// }
-
-// function getCustomer(id: number): Customer | null | undefined {
-//     return id === 0 ? null : {name: "Oluwapelumi", id}
-// }
-
-// const customerDto= getCustomer(0);
-// console.log(customerDto?.name)
-
-// let log= null
-
-// // let phone= <HTMLInputElement> document.getElementById("Phone")
-
-// class Account {
-//     readonly id: number;
-//     holder: string;
-//     private _balance: number;
-//     constructor(id: number, holder: string, balance: number){
-//         this.id= id;
-//         this.holder= holder;
-//         this._balance= balance;
-//     }
-//     deposit(amount: number): void {
-//         if(amount < 0)  throw new Error('Invalid Amount');
-//         this._balance+=amount;
-//     }
-//     get balance (){
-//      return this._balance;
-//     }
-// }
-// const account = new Account(0,"Orebayo Oluwapelumi",40000);
-// account.deposit(1000);
-// // console.log(account.balance);
-
-
-// class seatAssignment {
-//     [seatNumber: string]: string
-// }
-// const seats= new seatAssignment()
-// seats.A2="pelumi";
-// console.log(seats);
-
-import tensor from "@tensorflow/tfjs-node";
+const server = http.createServer(app);
+const port = normalizePort(process.env.PORT as string);
+server.on("listening", () => {
+  const address= server.address();
+  const bind = typeof address === "string" ? `pipe ${port}` : `port ${port}`;
+  console.log(`listening on ${bind}`);
+  const log= "[?...] Connecting"
+  console.log(log);
+});
+server.listen(port);
